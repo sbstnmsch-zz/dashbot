e4c83da8efac2a3e47076fc9388999b4ea712c40
smisch <sebastian.misch@epost-dev.de>
2015-01-09 17:13:05 +0100

    Add units to number widget

--- app/javascript/visuals/number-controller.js
+++ app/javascript/visuals/number-controller.js
@@ -12,13 +12,30 @@ module.exports = {
   fn: function($scope, $timeout, $http) {
     'use strict';
 
-    var _getJson = function() {
+    var
+    _format = function() {
+      if ($scope.visual.unit) {
+        if ($scope.visual.unit === 'bytes') {
+          if ($scope.value < 1024) {
+            $scope.unit = 'b';
+          } else if ($scope.value < 1024 * 1024) {
+            $scope.unit = 'kb';
+            $scope.value = Math.round($scope.value / 1024);
+          } else {
+            $scope.unit = 'mb';
+            $scope.value = Math.round($scope.value / (1024 * 1024));
+          }
+        }
+      }
+    },
+
+    _getJson = function() {
       var apiURL = $scope.visual.xhr;
       $scope.visual.loading = true;
       $http.get(apiURL)
         .success(function(data) { // jshint ignore:line
           $scope.value = eval('data.' + $scope.visual.xhrValue); // jshint ignore:line
-
+          _format();
           if ($scope.visual.green && $scope.visual.red) {
             $scope.visual.build = eval( // jshint ignore:line
               $scope.value + $scope.visual.green + '? "green" : (' +
@@ -40,6 +57,7 @@ module.exports = {
         _getJson();
       } else {
         $scope.value = $scope.visual.value;
+        _format();
       }
     }
   }
